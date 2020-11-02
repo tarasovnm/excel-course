edef87b5045193626772446fe6b6cfe0c5874cf4
tarasovnm <tarasovnm@gmail.com>
2020-11-03 00:43:59 +0400

    Implemented columns and rows resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,7 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from '@/components/table/table.template';
+import { resizeHandler } from '@/components/table/table.resize';
+import { shouldResize } from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -15,8 +17,8 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      console.log('Start resizing', event.target.dataset.resize);
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
     }
   }
 }
