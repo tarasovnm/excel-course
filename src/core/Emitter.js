50dc5e00cbd437c6edfba5ec8b03a5dc8b9591c1
tarasovnm <tarasovnm@gmail.com>
2020-11-07 12:29:09 +0400

    Finished base table logic

---
+++ src/core/Emitter.js
@@ -0,0 +1,33 @@
+export class Emitter {
+  constructor() {
+    this.listeners = {
+    }
+  }
+
+  // Уведомляем слушателей если они есть
+  // table.emit('table:select', {a: 1})
+  emit(event, ...args) {
+    if (!Array.isArray(this.listeners[event])) {
+      return false;
+    }
+
+    this.listeners[event].forEach(listener => {
+      listener(...args);
+    });
+
+    return true;
+  }
+
+  // Подписываемся на уведомление
+  // Добавляем нового слушателя
+  // formula.subscribe('table:select', () => {})
+  subscribe(event, fn) {
+    this.listeners[event] = this.listeners[event] || [];
+    this.listeners[event].push(fn);
+
+    return () => {
+      this.listeners[event] =
+        this.listeners[event].filter(listener => listener !== fn);
+    }
+  }
+}
